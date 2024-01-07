a28f7a0497e5f36021255054c9792b4d9ddf6cde
Simranjeet Singh <simranjeet@s25.digital>
2024-01-07 17:55:58 +0530

    [Adding client class]

--- src/client.ts
+++ src/client.ts
@@ -21,6 +21,55 @@ class CentralRegistry {
   }
 
   private async _generateToken() {
-    
+    try {
+      const url = `${this._baseUrl}/auth/realms/sahamati/protocol/openid-connect/token`;
+      const res = await this._httpClient.request({
+        url,
+        method: "POST",
+        headers: {
+          authorization: `Basic ${this._basicAuth}`,
+          "Content-Type": "appliccation/x-www-form-urlencoded",
+        },
+        data: new URLSearchParams({
+          grant_type: "client_credentials",
+          scope: "openid",
+        }),
+      });
+
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  private async _getEntityInfo(type: string) {
+    const url = `${this._baseUrl}/entityInfo/${type}/`;
+    const token = await this.getToken();
+    const res = await this._httpClient.request({
+      url,
+      method: "GET",
+      headers: {
+        authorization: token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    return res;
+  }
+
+  public async getToken() {
+    return await this._generateToken();
+  }
+
+  public async getAA() {
+    return await this._getEntityInfo(EntityType.AA);
+  }
+
+  public async getFIP() {
+    return await this._getEntityInfo(EntityType.FIP);
+  }
+
+  public async getFIU() {
+    return await this._getEntityInfo(EntityType.FIU);
   }
 }
